9c99998e0048642243ea7d942b046203090d111e
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-20 22:45:50 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,14 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+    }
 
-  
+
 end
